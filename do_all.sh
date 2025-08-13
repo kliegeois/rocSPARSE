8794e94ac9584a4ebc536749613d88a23df85229
kliegeois <kimliegeois@ymail.com>
2025-08-13 15:39:08 -0600

    tmp

--- do_all.sh
+++ do_all.sh
@@ -1,6 +1,11 @@
 pwd_path=`pwd`
 
-./install.sh -c -a gfx942
+if [ ! -d "$pwd_path/build/release" ]; then
+    ./install.sh -c -a gfx942
+else
+    cd $pwd_path/build/release
+    make -j 10
+fi
 
 cd $pwd_path/scripts/performance/matrices
 ./build_convert.sh
@@ -10,7 +15,7 @@ cd $pwd_path/build/release/clients/staging
 
 rm -rf $pwd_path/log.txt
 
-for i in {1..1000}; do
+for i in {1..100}; do
     ./rocsparse-bench -f csrmv --precision s --device 0 --alpha 1 --beta 0 --iters 20 --rocalution $pwd_path/scripts/performance/matrices/mc2depi.csr | grep "NT             " >> $pwd_path/log.txt
 done
 
