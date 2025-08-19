665077d63f848b49e808c39f60cf3309c95afd63
kliegeois <kimliegeois@ymail.com>
2025-08-19 07:43:37 -0600

    tmp

--- do_all.sh
+++ do_all.sh
@@ -1,4 +1,5 @@
 pwd_path=`pwd`
+temp_path=/tmp/grpc/
 
 if [ ! -d "$pwd_path/build/release" ]; then
     yum install python38
@@ -30,7 +31,7 @@ rm -rf pmc_*
 for i in {1..100}; do
     echo -e "start ${i}"
     rocprofv2 --plugin file -o csrmv_mc2depi_${i} -i ${pwd_path}/counters.txt ./rocsparse-bench --transposeA N -f csrmv --precision s --device 0 --alpha 1 --beta 0 --iters 20 --rocalution $pwd_path/scripts/performance/matrices/mc2depi.csr >> $pwd_path/log_prof.txt
-    tar -cvzf results.tar.gz workloads/* pmc_* log.txt log_prof.txt
+    tar -cvzf ${temp_path}results.tar.gz workloads/* pmc_* log.txt log_prof.txt
     echo -e "run ${i} done"
 done
 
@@ -44,10 +45,10 @@ cd $pwd_path
 #for i in {1..20}; do
 #    echo -e "start ${i}"
 #    python3.8 /opt/rocm-7.0.0/bin/rocprof-compute profile -n csrmv_mc2depi_${i} -- ./build/release/clients/staging/rocsparse-bench --transposeA N -f csrmv --precision s --device 0 --alpha 1 --beta 0 --iters 20 --rocalution $pwd_path/scripts/performance/matrices/mc2depi.csr >> $pwd_path/log_prof.txt
-#    tar -cvzf results.tar.gz workloads/* pmc_* log.txt log_prof.txt
+#    tar -cvzf ${temp_path}results.tar.gz workloads/* pmc_* log.txt log_prof.txt
 #    echo -e "run ${i} done"
 #done
 
-tar -cvzf results.tar.gz workloads/* pmc_* log.txt log_prof.txt
+tar -cvzf ${temp_path}results.tar.gz workloads/* pmc_* log.txt log_prof.txt
 
 python3 $pwd_path/read_python.py
